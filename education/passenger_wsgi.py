3a4833ece2dbe4c2997d061cc21dcfb30a9dd166
hvids <hvidsmen@mail.ru>
2025-05-05 18:09:24 +0300

    ass

--- education/passenger_wsgi.py
+++ education/passenger_wsgi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u3122635/data/www/iprofpromeducation.pro')
+sys.path.insert(0, '/var/www/u3122635/data/www/iprofpromeducation.pro/education')
 sys.path.insert(1, '/var/www/u3122635/data/djangoenv/lib/python3.6/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'education.settings'
 from django.core.wsgi import get_wsgi_application
