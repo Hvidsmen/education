9102635b4560007159c9bb6626841b14720fa7fb
hvids <hvidsmen@mail.ru>
2025-05-06 13:44:24 +0300

    add

--- education/settings.py
+++ education/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATIC_ROOT =BASE_DIR / "static/"
 MEDIA_ROOT = '/media/'
 MEDIA_URL = 'media/'
 
--- education/urls.py
+++ education/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
                   path('', include('course.urls')),
                   path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
                   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
